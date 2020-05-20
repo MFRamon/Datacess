446b0c037d412c4c6f6dc77a0436ba6098828571
Ramon Manrique <ramon.manfig@gmail.com>
2020-05-19 22:18:55 -0500

    Solution for Page 1

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   API: {
     EndPoint: {
-      Northwind: 'https://localhost:44337/v1/analysis/northwind/'
+      Northwind: 'https://localhost:44315/v1/analysis/northwind/'
     }
   }
 };
