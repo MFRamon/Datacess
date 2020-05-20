01c5f5fb360324dc622a3b2883bd0886a33b47cf
Ramon Manrique <ramon.manfig@gmail.com>
2020-05-19 21:52:26 -0500

    Some pending changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './pages/home/home.component';
+import { CalculateComponent } from './pages/calculate/calculate.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'barras', component: HomeComponent },
+  { path: 'pie', component: CalculateComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+imports: [RouterModule.forRoot(routes, { useHash: true })],
+exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
+
